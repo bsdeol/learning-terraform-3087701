98c4ad43655c5d5ab681436dd0bbd86323bfaebd
bsdeol <108632450+bsdeol@users.noreply.github.com>
2025-09-10 11:31:18 -0700

    auto scalling group

--- main.tf
+++ main.tf
@@ -34,26 +34,27 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-  #vpc_security_group_ids = [aws_security_group.blog.id]
-  vpc_security_group_ids  = [module.blog_sg.security_group_id]
- 
-  subnet_id               = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source    = "terraform-aws-modules/autoscaling/aws"
+  version   = "9.0.1"
+  # insert the 1 required variable here
+  name      = "blog"
+  min_size  = 1
+  max_size  = 2
 
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = modele.blog_alb.target_group_arns
+  secuirty_groups  = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  image_id              = data.aws_ami.app_ami.id
+  instance_type    = var.instance_type
 }
 
 #Application Load Balanceer
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
-  
+
   name                = "blog-alb"
 
   load_balancer_type  = "application"
