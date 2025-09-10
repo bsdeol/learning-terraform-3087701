54b376f3681bd5b53bcc52fdf4f7adb0297e520a
bsdeol <108632450+bsdeol@users.noreply.github.com>
2025-09-10 10:37:45 -0700

    added [] for security groups

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ module "blog_alb" {
   
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_group  = module.blog_sg.security_group_id
+  security_group  = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
