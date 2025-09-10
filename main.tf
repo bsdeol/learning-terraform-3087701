6b43257cbca14112b94807f98012f85dc13cc3e9
bsdeol <108632450+bsdeol@users.noreply.github.com>
2025-09-10 11:13:10 -0700

    fixed on 11:13

--- main.tf
+++ main.tf
@@ -53,14 +53,13 @@ resource "aws_instance" "blog" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  
   name                = "blog-alb"
 
   load_balancer_type  = "application"
   
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_group  = [module.blog_sg.security_group_id]
+  security_groups  = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -68,6 +67,13 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
+      
+      targets = {
+        my_target = {
+             target_id = aws_instance.blog.id
+              port = 80
+        }
+      }
     }
   ]
 
