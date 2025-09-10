4f89472d1d56408053803ce59e63eafe2d9601d1
bsdeol <108632450+bsdeol@users.noreply.github.com>
2025-09-09 18:41:46 -0700

    output public ip

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.blog.public_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.blog.public_dns
+}
